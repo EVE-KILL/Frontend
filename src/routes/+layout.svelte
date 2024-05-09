f170cebc45b6b4fb45f20a17ab531f31b0591a2e
Michael Karbowiak <michael@karbowiak.dk>
2024-05-09 16:34:22 +0200

    Add bootstrap

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,38 @@
 <script>
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 	import Navbar from './Navbar.svelte';
 	import './styles.css';
+
+	onMount(async () => {
+		if (!browser) return;
+
+		// this is enough for most components
+		await import('bootstrap');
+
+		// some components require a bootstrap instance, to fulfil their job. In that case, use this:
+		// const bootstrap = await import("bootstrap");
+		// sample usage:
+		// const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+	});
 </script>
 
 <!-- Back To Top -->
-<div class="backtotop" style="position: fixed; margin-bottom: 25px; margin-right: 50px; bottom: 0; right: 0" alt="Back to Top">
+<div
+	class="backtotop"
+	style="position: fixed; margin-bottom: 25px; margin-right: 50px; bottom: 0; right: 0"
+	alt="Back to Top"
+>
 	<a style="position: relative; bottom: 0; right: 0;" href="#top">
-		<img src="/img/arrow-up.png" title="Back to Top">
+		<img src="/img/arrow-up.png" title="Back to Top" />
 	</a>
 </div>
 
 <!-- Popup -->
-<div id="popup" style="display: none; position: absolute; top: 217px; width: 99%; z-index: 3; padding: 5px;"/>
+<div
+	id="popup"
+	style="display: none; position: absolute; top: 217px; width: 99%; z-index: 3; padding: 5px;"
+></div>
 
 <!-- Header Text/Image-->
 <div style="text-align: center;">
@@ -23,7 +44,7 @@
 	<div id="contentkb">
 		<section>
 			<!-- Navbar -->
-			<Navbar/>
+			<Navbar></Navbar>
 
 			<!-- Dropdowns -->
 
@@ -38,27 +59,26 @@
 				<div id="boardmessage">SOME MESSAGE</div>
 
 				<!-- Right side main content -->
-				<div id="content" style="width: 100%">
-					content here!
-				</div>
+				<div id="content" style="width: 100%">content here!</div>
 
 				<!-- Left side content -->
-				<div id="context">
-					context!
-				</div>
+				<div id="context">context!</div>
 
 				<div id="footerarea">
-					<br>
+					<br />
 					<div class="counter"></div>
-					<br>
+					<br />
 					<div class="footerbacktotop2" style="float: left">
-						<a href="http://evekb.org/forum/viewtopic.php?f=1033&t=18159">EVE+</a> theme by <a href="http://gate.eveonline.com/profile/vecati">Vecati</a>
-						<br>
+						<a href="http://evekb.org/forum/viewtopic.php?f=1033&amp;t=18159">EVE+</a> theme by
+						<a href="http://gate.eveonline.com/profile/vecati">Vecati</a>
+						<br />
 						Modified for Thessia by Karbowiak
 					</div>
 				</div>
-
 			</section>
 		</section>
 	</div>
 </div>
+<slot></slot>
+
+<style></style>
