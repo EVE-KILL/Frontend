7a90c6bac4d079a56541da2e99da4af6c2e52e9a
Michael Karbowiak <michael@karbowiak.dk>
2024-05-09 02:22:53 +0200

    Attempt some flexbox thing

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,12 +1,10 @@
-<script>
-
-</script>
-<div id="main_menu">
-	<ul>
+<div id="main_menu" class="flex-container">
+	<ul class="flex-items">
 		<div class="navigation-section">
 			<li><a class="link" href="/">Home</a></li>
 			<li class="dropdown">Kills
 				<div class="dropdown-content">
+					<!-- dropdown content here -->
 					<table>
 						<tr>
 							<th><a class="link" href="/latest/">Latest</a></th>
@@ -54,28 +52,41 @@
 			<li><a class="link" href="#">Wars</a></li>
 			<li><a class="link" href="#">Map</a></li>
 			<li><a class="link" href="#">Top</a></li>
-			<li>
+
+			<li class="flex-grow">
 				<form class="searchBox" method="get" action="#">
 					<input class="typeahead" type="text" name="searchAutocomplete" id="searchBox" size="29" placeholder="Search" tabindex="1">
 				</form>
 			</li>
-			<li style="float: right"><a class="link" href="#">Login</a></li>
-			<li style="float: right;" class="dropdown">Information
+
+			<!-- Align right -->
+			<li><a class="link" href="#">Login</a></li>
+			<li class="dropdown">Information
 				<div class="dropdown-content">
-					<table>
-						<tr>
-							<th><a class="link" href="/about/">About</a></th>
-						</tr>
-						<tr>
-							<th><a class="link" href="#">Stats</a></th>
-						</tr>
-						<tr>
-							<th><a class="link" href="#">FAQ</a></th>
-						</tr>
-					</table>
+					<!-- dropdown content here -->
 				</div>
 			</li>
-			<li style="float: right"><a class="link" href="javascript:void(0)" onclick="toggle('togglePost')">Post Killmail</a></li>
 		</div>
 	</ul>
 </div>
+
+<style>
+    .flex-container {
+        display: flex;
+    }
+
+    .flex-items {
+        display: flex;
+        flex-wrap: nowrap;
+        padding: 0;
+        margin: 0;
+    }
+
+    .flex-grow {
+        flex-grow: 1;
+    }
+
+    .flex-grow input {
+        width: 100%;
+    }
+</style>
