4a767a9b77061d21299dbc29ec4192bb262fa5b0
Michael Karbowiak <michael@karbowiak.dk>
2024-06-11 00:07:22 +0200

    Initial commit

---
+++ src/lib/fetchKillList.js
@@ -0,0 +1,13 @@
+const killListURL = 'https://eve-kill.com/api/killlist/latest';
+const pageId = 1;
+
+export async function fetchKillList(pageNumber = 1) {
+	const url = `${killListURL}/${pageNumber}`;
+
+	try {
+		const response = await fetch(url);
+		return await response.json();
+	} catch (error) {
+		console.error('Error fetching kill list:', error);
+	}
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,svelte,ts}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Shentox', 'sans-serif']
+      },
+      colors: {
+        dark: '#121212', // base dark color
+        darker: '#0a0a0a' // darker shade
+      },
+      backgroundColor: {
+        'semi-transparent': 'rgba(0, 0, 0, 0.85)' // semi-transparent black
+      }
+    },
+  },
+  plugins: [],
+}
+
