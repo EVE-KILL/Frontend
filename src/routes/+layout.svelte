89d9af7ca074a8aa6ea0d320edfbcf2ce8705058
Michael Karbowiak <michael@karbowiak.dk>
2024-06-12 19:13:41 +0200

    Make the background follow along the entire way down

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,24 @@
     import Navbar from '../components/Navbar.svelte';
 </script>
 
-<div id="content" class="content flex flex-col h-screen mx-auto max-w-7xl">
+<div id="content" class="content flex flex-col mx-auto max-w-7xl">
     <div id="inner-content" class="inner-content">
         <Navbar />
 
         <div id="page-title" class="page-title pb-2 ml-5">
             <div class="text-white text-2xl font-bold">Frontpage</div>
         </div>
-        <slot />
+
+        <div class="container flex p-2 pt-4 gap-2">
+            <slot />
+        </div>
     </div>
 </div>
+
+<style>
+    .content {
+        background-color: rgba(21, 21, 21, 0.7);
+        border: 2px solid #252525;
+        border-radius: 10px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,33 +8,23 @@
     const url = "https://eve-kill.com/api/killlist/latest";
 </script>
 
-<div class="container flex h-screen p-2 pt-4 gap-2">
-    <div class="w-1/4">
-        <div class="pb-5">
-            <Characters url="https://eve-kill.com/api/stats/top10characters" />
-        </div>
-        <div class="pb-5">
-            <Corporations url="https://eve-kill.com/api/stats/top10corporations" />
-        </div>
-        <div class="pb-5">
-            <Alliances url="https://eve-kill.com/api/stats/top10alliances" />
-        </div>
-        <div class="pb-5">
-            <Systems url="https://eve-kill.com/api/stats/top10solarsystems" />
-        </div>
-        <div class="pb-5">
-            <Regions url="https://eve-kill.com/api/stats/top10regions" />
-        </div>
+<div class="w-1/4">
+    <div class="pb-5">
+        <Characters url="https://eve-kill.com/api/stats/top10characters" />
     </div>
-    <div class="w-full">
-        <KillList {url} />
+    <div class="pb-5">
+        <Corporations url="https://eve-kill.com/api/stats/top10corporations" />
+    </div>
+    <div class="pb-5">
+        <Alliances url="https://eve-kill.com/api/stats/top10alliances" />
+    </div>
+    <div class="pb-5">
+        <Systems url="https://eve-kill.com/api/stats/top10solarsystems" />
+    </div>
+    <div class="pb-5">
+        <Regions url="https://eve-kill.com/api/stats/top10regions" />
     </div>
 </div>
-
-<style>
-    .container {
-        background-color: rgba(21, 21, 21, 0.7);
-        border: 2px solid #252525;
-        border-radius: 10px;
-    }
-</style>
+<div class="w-full">
+    <KillList {url} />
+</div>
