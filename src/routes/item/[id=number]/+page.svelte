1e4a370ec720facdfe5fb96b71b2550d244709bc
Michael Karbowiak <michael@karbowiak.dk>
2024-08-02 23:57:48 +0200

    Make the tables look more like the attackers table and the other tables

--- src/routes/item/[id=number]/+page.svelte
+++ src/routes/item/[id=number]/+page.svelte
@@ -11,7 +11,7 @@
     let itemKillmailsUrl = `${upstreamUrl}/api/items/${data.id}/killmails/20`;
     let killmailsUrl = `${upstreamUrl}/api/killmail/`;
 
-    let item = [];
+    let item = {};
     let killmails: Killmail[] = [];
     let prices = [];
 
@@ -21,7 +21,6 @@
         item = await response.json();
         prices = await responsePrice.json();
 
-        // Use the POST api to send the list of killmail_ids from killmailsIds to the killmails endpoint, to retrieve the full killmails
         const responseKillmails = await fetch(itemKillmailsUrl);
         let killmailsIds = await responseKillmails.json();
         const postKillmails = await fetch(killmailsUrl, {
@@ -52,49 +51,57 @@
     </div>
 
     <div class="flex space-x-4">
-        <div class="w-1/2 p-4 rounded">
+        <div class="w-1/2 p-4 rounded bg-opacity-75">
             <h2 class="text-xl font-bold mb-4">Latest Kills</h2>
             <div>
                 {#each killmails as killmail}
-                <a href="/kill/{killmail.killmail_id}" rel="tooltip" title="Detail for {killmail.killmail_id}" class="flex items-center mb-4 cursor-pointer no-underline text-white">
-                    <img src="{killmail.victim.ship_image_url}" style="height: 64px; width: 64px;" class="rounded mr-2" alt="{killmail.victim.ship_name}">
-                    <a href="/character/{killmail.victim.character_id}" rel="tooltip" title="{killmail.victim.character_name}" class="mr-2">
-                        <img src="{killmail.victim.character_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.character_name}">
-                    </a>
-                    <a href="/corporation/{killmail.victim.corporation_id}" rel="tooltip" title="{killmail.victim.corporation_name}" class="mr-2">
-                        <img src="{killmail.victim.corporation_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.corporation_name}">
-                    </a>
-                    <a href="/alliance/{killmail.victim.alliance_id}" rel="tooltip" title="{killmail.victim.alliance_name}" class="mr-2">
-                        <img src="{killmail.victim.alliance_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.alliance_name}">
-                    </a>
-                    <div>
-                        {killmail.victim.character_name} ({killmail.victim.ship_name})<br>
-                        {killmail.victim.corporation_name}<br>
-                        {killmail.victim.alliance_name}
+                <a href="/kill/{killmail.killmail_id}" rel="tooltip" title="Detail for {killmail.killmail_id}" class="block mb-4 cursor-pointer no-underline text-white">
+                    <div class="flex items-center">
+                        <img src="{killmail.victim.ship_image_url}" style="height: 64px; width: 64px;" class="rounded mr-2" alt="{killmail.victim.ship_name}">
+                        <div class="mr-2">
+                            <a href="/character/{killmail.victim.character_id}" rel="tooltip" title="{killmail.victim.character_name}">
+                                <img src="{killmail.victim.character_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.character_name}">
+                            </a>
+                        </div>
+                        <div class="mr-2">
+                            <a href="/corporation/{killmail.victim.corporation_id}" rel="tooltip" title="{killmail.victim.corporation_name}">
+                                <img src="{killmail.victim.corporation_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.corporation_name}">
+                            </a>
+                        </div>
+                        <div class="mr-2">
+                            <a href="/alliance/{killmail.victim.alliance_id}" rel="tooltip" title="{killmail.victim.alliance_name}">
+                                <img src="{killmail.victim.alliance_image_url}" style="height: 64px; width: 64px;" class="rounded" alt="{killmail.victim.alliance_name}">
+                            </a>
+                        </div>
+                        <div>
+                            {killmail.victim.character_name} ({killmail.victim.ship_name})<br>
+                            {killmail.victim.corporation_name}<br>
+                            {killmail.victim.alliance_name}
+                        </div>
                     </div>
                 </a>
                 {/each}
             </div>
         </div>
-        <div class="w-1/2 p-4 rounded">
+        <div class="w-1/2 p-4 rounded bg-opacity-75">
             <h2 class="text-xl font-bold mb-4">Market Prices - The Forge</h2>
             <div>
-                <table class="table-auto w-full text-left text-sm">
+                <table class="table-auto w-full text-left text-sm bg-semi-transparent rounded-lg shadow-lg">
                     <thead>
-                        <tr>
-                            <th class="border-b border-gray-600 py-2">Date</th>
-                            <th class="border-b border-gray-600 py-2 text-right">Min Price</th>
-                            <th class="border-b border-gray-600 py-2 text-right">Avg Price</th>
-                            <th class="border-b border-gray-600 py-2 text-right">Max Price</th>
+                        <tr class="bg-darker text-white uppercase text-xs leading-normal">
+                            <th class="px-2 py-1">Date</th>
+                            <th class="px-2 py-1 text-right">Min Price</th>
+                            <th class="px-2 py-1 text-right">Avg Price</th>
+                            <th class="px-2 py-1 text-right">Max Price</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="text-gray-300 text-sm">
                         {#each prices as price}
-                        <tr>
-                            <td class="border-b border-gray-600 py-2">{price.date}</td>
-                            <td class="border-b border-gray-600 py-2 text-right">{formatNumber(price.lowest)} ISK</td>
-                            <td class="border-b border-gray-600 py-2 text-right">{formatNumber(price.average)} ISK</td>
-                            <td class="border-b border-gray-600 py-2 text-right">{formatNumber(price.highest)} ISK</td>
+                        <tr class="border-b border-gray-700 hover:bg-gray-600 transition-colors duration-300">
+                            <td class="px-2 py-1">{price.date}</td>
+                            <td class="px-2 py-1 text-right">{formatNumber(price.lowest)} ISK</td>
+                            <td class="px-2 py-1 text-right">{formatNumber(price.average)} ISK</td>
+                            <td class="px-2 py-1 text-right">{formatNumber(price.highest)} ISK</td>
                         </tr>
                         {/each}
                     </tbody>
