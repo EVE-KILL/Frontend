852dadd2a42469e66cb9676a55b20e2b858c2964
Michael Karbowiak <michael@karbowiak.dk>
2024-08-03 16:02:49 +0200

    Remove /build/ from the path for the handler

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { createProxyMiddleware } from 'http-proxy-middleware';
-import { handler } from './build/handler.js';
+import { handler } from './handler.js';
 
 const app = express();
 
