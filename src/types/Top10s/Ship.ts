e14f94c362ad47e759a9cb21d346646c141ffcaa
Michael Karbowiak <michael@karbowiak.dk>
2024-08-03 23:45:21 +0200

    Add ship type

---
+++ src/types/Top10s/Ship.ts
@@ -0,0 +1,16 @@
+export interface Ship {
+	count: number;
+	type_id: number;
+	capacity: number;
+	description: string;
+	graphic_id: number;
+	group_id: number;
+	market_group_id: number;
+	mass: number;
+	name: string;
+	packaged_volume: number;
+	portion_size: number;
+	published: boolean;
+	radius: number;
+	volume: number;
+}
