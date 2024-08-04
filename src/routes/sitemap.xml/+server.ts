68296fd1cd77974bc8e8ad8dcbfd74dce7527a93
Michael Karbowiak <michael@karbowiak.dk>
2024-08-04 17:49:05 +0200

    Add sitemap output

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,23 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { getUpstreamUrl } from '$lib/Config';
+
+export const GET: RequestHandler = async () => {
+    const upstreamUrl = getUpstreamUrl();
+    let sitemapUrl = `${upstreamUrl}/api/sitemap`;
+    const response = await fetch(sitemapUrl);
+    const data = await response.json();
+
+    const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${data.links.map((link: string) => `
+            <sitemap>
+                <loc>${`https://yourdomain.com${link}.xml`}</loc>
+            </sitemap>`).join('')}
+    </sitemapindex>`;
+
+    return new Response(sitemapIndex, {
+        headers: {
+            'Content-Type': 'application/xml'
+        }
+    });
+};
---
+++ src/routes/sitemap/[id].xml/+server.ts
@@ -0,0 +1,31 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { getUpstreamUrl } from '$lib/Config';
+
+export const GET: RequestHandler = async ({ params }) => {
+    const { id } = params;
+    const upstreamUrl = getUpstreamUrl();
+    let sitemapUrl = `${upstreamUrl}/api/sitemap/${id}`;
+    const response = await fetch(sitemapUrl);
+    const data = await response.json();
+
+    const subSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        ${data.map((item: any) => `
+            <url>
+                <loc>${`https://yourdomain.com${item.loc}`}</loc>
+                <changefreq>${item.changefreq}</changefreq>
+                <priority>${item.priority}</priority>
+                <lastmod>${item.lastmod}</lastmod>
+                <image:image>
+                    <image:loc>${item.image}</image:loc>
+                    <image:title>${item.name}</image:title>
+                </image:image>
+            </url>`).join('')}
+    </urlset>`;
+
+    return new Response(subSitemap, {
+        headers: {
+            'Content-Type': 'application/xml'
+        }
+    });
+};
