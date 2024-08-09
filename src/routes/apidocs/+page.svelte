c3c425b7ec9afde928845e93a2138fe72f6268ee
Michael Karbowiak <michael@karbowiak.dk>
2024-08-09 20:04:49 +0200

    Remove the react component and use the plain api-reference from jsdelivr

--- src/routes/apidocs/+page.svelte
+++ src/routes/apidocs/+page.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     import { getUpstreamUrl } from '$lib/Config';
-    import { ApiReferenceReact } from '@scalar/api-reference-react';
-
     const upstreamUrl = getUpstreamUrl();
+
+    // Template literal for multi-line HTML content
+    let html = `
+        \<script id="api-reference" data-url="${upstreamUrl}/api/openapi"\>\</script\>
+        \<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"\>\</script\>
+    `;
 </script>
 
-<react:ApiReferenceReact configuration={{spec: {url: `${upstreamUrl}/api/openapi`}}}/>
+{@html html}
