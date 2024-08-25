b6e94b6f2c2ccb89c182976536f78755fe054ebb
Michael Karbowiak <michael@karbowiak.dk>
2024-08-26 00:07:32 +0200

    Remove the images.eve-kill.com handling, and refer the healthcheck to the healthcheck route

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,24 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
 import { parse, serialize } from 'cookie';
-import { LRUCache } from 'lru-cache';
-import crypto from 'crypto';
-
-type CachedResponse = {
-	statusCode: number;
-	headers: Record<string, string>;
-	body: Buffer;
-};
-
-// Initialize cache with a maximum size of 2GB
-const cache = new LRUCache<string, CachedResponse>({
-	maxSize: 2 * 1024 * 1024 * 1024, // 2GB in bytes
-	sizeCalculation: (value) => value.body.length,
-	ttl: 60 * 60 * 24 * 7, // 1 week
-	allowStale: true
-});
-
-// Helper function to create a hash of the cache key
-const hashKey = (key: string) => crypto.createHash('sha256').update(key).digest('hex');
 
 // Function to handle CORS
 const setCorsHeaders = (headers: Headers) => {
@@ -33,60 +14,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const session = cookies.session ? JSON.parse(cookies.session) : null;
 	event.locals.user = session ? session.user : null;
 
-	// Handle healthcheck route
-	if (event.url.pathname === '/healthcheck') {
-		return new Response('ok', { status: 200 });
-	}
-
 	// Handle favicon request
 	if (event.url.pathname === '/favicon') {
 		return new Response(await fetch('/static/favicon.png'));
-	}
-
-	// Handle requests to images.eve-kill.com
-	if (event.url.hostname === 'images.eve-kill.com') {
-		const targetUrl = `https://images.evetech.net${event.url.pathname}`;
-		const cacheKey = hashKey(event.url.pathname);
-
-		if (cache.has(cacheKey)) {
-			// Serve from cache
-			const cachedResponse = cache.get(cacheKey);
-			const responseHeaders = new Headers(cachedResponse!.headers);
-			setCorsHeaders(responseHeaders);
-
-			return new Response(cachedResponse!.body, {
-				status: cachedResponse!.statusCode,
-				headers: responseHeaders
-			});
-		}
-
-		try {
-			const proxyRes = await fetch(targetUrl, {
-				method: event.request.method,
-				headers: event.request.headers
-			});
-
-			const bodyBuffer = Buffer.from(await proxyRes.arrayBuffer());
-			const responseHeaders = Object.fromEntries(proxyRes.headers.entries());
-
-			// Cache the response
-			cache.set(cacheKey, {
-				statusCode: proxyRes.status,
-				headers: responseHeaders,
-				body: bodyBuffer
-			});
-
-			// Return the proxied response
-			return new Response(bodyBuffer, {
-				status: proxyRes.status,
-				headers: new Headers({
-					...responseHeaders,
-					'Cache-Control': 'public, max-age=31536000' // 1 year caching
-				})
-			});
-		} catch (error) {
-			return new Response(error.message, { status: 500 });
-		}
 	}
 
 	// Resolve the request and generate the response
