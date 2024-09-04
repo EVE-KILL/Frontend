8519db6f72b9a5ec38234c609353b5e637ef2d1b
Michael Karbowiak <michael@karbowiak.dk>
2024-09-04 15:07:58 +0200

    Move the battle info and external links to the same line

--- src/components/Kill/Navbar.svelte
+++ src/components/Kill/Navbar.svelte
@@ -31,33 +31,32 @@
 
 <div class="mt-4">
     <nav class="bg-semi-transparent text-white py-2 px-4 rounded">
-        <ul class="flex space-x-4 justify-start">
-            <!-- Dropdown with DOTLAN links -->
-            <li>
-                <div class="relative">
-                    <button class="text-white hover:text-gray-400 focus:outline-none" on:mouseenter={openExternalDropdown} on:mouseleave={closeExternalDropdown}>
-                        External
-                    </button>
-                    {#if isExternalDropDownOpen}
-                        <ul class="absolute left-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10" on:mouseenter={openExternalDropdown} on:mouseleave={closeExternalDropdown}>
-                            <li class="px-4 py-2 text-sm text-gray-300">DOTLAN</li>
-                            <li><a href="https://evemaps.dotlan.net/system/{killmail.system_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.system_name}</a></li>
-                            <li><a href="https://evemaps.dotlan.net/region/{killmail.region_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
-                            <li class="px-4 py-2 text-sm text-gray-300">EVEEye</li>
-                            <li><a href="https://eveeye.com/?m={killmail.region_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
-                            <li class="px-4 py-2 text-sm text-gray-300">EVE Missioneer</li>
-                            <li><a href="https://evemissioneer.com/s/{killmail.system_id}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.system_name}</a></li>
-                            <li><a href="https://evemissioneer.com/r/{killmail.region_id}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
-                            <li class="px-4 py-2 text-sm text-gray-300">EveShip.fit</li>
-                            <li><a href="https://eveship.fit/?fit=killmail:{killmail.killmail_id}/{killmail.hash}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">ESF</a></li>
-                        </ul>
-                    {/if}
-                </div>
+        <!-- Combine both menus into the same flex container -->
+        <ul class="flex space-x-4 justify-between w-full">
+            <!-- Left aligned items (External dropdown) -->
+            <li class="relative">
+                <button class="text-white hover:text-gray-400 focus:outline-none" on:mouseenter={openExternalDropdown} on:mouseleave={closeExternalDropdown}>
+                    External
+                </button>
+                {#if isExternalDropDownOpen}
+                    <ul class="absolute left-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10" on:mouseenter={openExternalDropdown} on:mouseleave={closeExternalDropdown}>
+                        <li class="px-4 py-2 text-sm text-gray-300">DOTLAN</li>
+                        <li><a href="https://evemaps.dotlan.net/system/{killmail.system_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.system_name}</a></li>
+                        <li><a href="https://evemaps.dotlan.net/region/{killmail.region_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
+                        <li class="px-4 py-2 text-sm text-gray-300">EVEEye</li>
+                        <li><a href="https://eveeye.com/?m={killmail.region_name}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
+                        <li class="px-4 py-2 text-sm text-gray-300">EVE Missioneer</li>
+                        <li><a href="https://evemissioneer.com/s/{killmail.system_id}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.system_name}</a></li>
+                        <li><a href="https://evemissioneer.com/r/{killmail.region_id}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">{killmail.region_name}</a></li>
+                        <li class="px-4 py-2 text-sm text-gray-300">EveShip.fit</li>
+                        <li><a href="https://eveship.fit/?fit=killmail:{killmail.killmail_id}/{killmail.hash}" target="_blank" class="block px-4 py-2 hover:bg-gray-700">ESF</a></li>
+                    </ul>
+                {/if}
             </li>
-        </ul>
-        <ul class="flex space-x-4 justify-end">
+
+            <!-- Right aligned items (Battle Info) -->
             {#if inBattle}
-                <li>
+                <li class="ml-auto">
                     <a href="/battle/kill/{killmail.killmail_id}" class="text-white hover:text-gray-400">Battle Info</a>
                 </li>
             {/if}
